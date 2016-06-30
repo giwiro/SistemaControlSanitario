9f0e845cc51e4d3318d54abe85f105dcbeaaad8e
Gi Wah <giwirodavalos@gmail.com>
2016-06-30 16:09:12 -0500

    cambios para generar jar. Aun no se pueden generar

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -11,10 +11,18 @@ import static spark.debug.DebugScreen.*;
 public class Application {
     public static void main(String[] args) {
 
-        port(8080);
+        port(getPort(8080));
         enableDebugScreen();
 
         get(Routes.Web.INDEX, IndexController.serveIndexPage);
         //get(Routes.Web.INDEX, (request, response) -> "Hello World");
+    }
+
+    private static int getPort(int defaultPort) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return defaultPort; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
